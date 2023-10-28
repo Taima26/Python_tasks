d5fcba9d00bdcd7b1bac8844f07c33c4b96f4675
Taima alkhateep <taima.alkhateep@gmail.com>
2023-10-28 14:48:35 +0200

    Data analysis

--- ToImport/orders.py
+++ ToImport/orders.py
@@ -6,14 +6,10 @@ class Order:
     counter=0
     def __init__(self, items , date=None):
         self.items = items
-<<<<<<< HEAD
         if date == None:
             self.date = datetime.now()
         else:
             self.date = date
-=======
-        self.date = datetime.now()
->>>>>>> de4e7bf77e509910915a4f5d3708bf4712662126
         self.order_id = f"order-{Order.counter}"
         Order.counter +=1
         self.order_accepted = self.is_order_accepted()
